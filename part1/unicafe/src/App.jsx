147a2ccc67b1f2fc92c6aa5ca4f6b8c7888c33a2
BradGrensewich <bradgrensewich@gmail.com>
2025-10-11 17:28:35 +0700

    exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,22 @@
 import { useState } from 'react';
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+const Statistics = ({ scores }) => {
+  const all = scores.good + scores.bad + scores.neutral;
+  const avg = (scores.good - scores.bad) / all;
+  const goodPercentage = (scores.good / all) * 100.0;
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>Good: {scores.good}</p>
+      <p>Neutral: {scores.neutral}</p>
+      <p>Bad: {scores.bad}</p>
+      <p>All: {all}</p>
+      <p>Average: {avg}</p>
+      <p>Positive: {goodPercentage}</p>
+    </div>
+  );
+};
 const App = () => {
   // save clicks of each button to its own state
   const [scores, setScores] = useState({
@@ -9,9 +26,7 @@ const App = () => {
   });
 
   //stats
-  const all = scores.good + scores.bad + scores.neutral;
-  const avg = (scores.good - scores.bad) / all;
-  const goodPercentage = scores.good / all * 100.0 
+
   const addClick = (click) => {
     setScores({ ...scores, [click]: scores[`${click}`] + 1 });
   };
@@ -22,14 +37,7 @@ const App = () => {
       <Button onClick={() => addClick('good')} text='good' />
       <Button onClick={() => addClick('neutral')} text='neutral' />
       <Button onClick={() => addClick('bad')} text='bad' />
-      <h2>Statistics</h2>
-      <p>Good: {scores.good}</p>
-      <p>Neutral: {scores.neutral}</p>
-      <p>Bad: {scores.bad}</p>
-      <p>All: {all}</p>
-      <p>Average: {avg}</p>
-      <p>Positive: {goodPercentage}</p>
-      
+      <Statistics scores={scores} />
     </div>
   );
 };
