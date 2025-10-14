bf3abc893d1e1048a2bc323d349d6d2929029656
BradGrensewich <bradgrensewich@gmail.com>
2025-10-14 21:01:20 +0700

    exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -43,14 +43,21 @@ const Form = ({ onAddNewPerson }) => {
   );
 };
 
-const PersonItem = ({person}) => {
+const PersonItem = ({ person }) => {
   return (
-    <li>{person.name} : {person.number}</li>
-  )
-}
+    <li>
+      {person.name} : {person.number}
+    </li>
+  );
+};
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: 123456775 }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
   const handleNewPersonAdded = (newPerson) => {
     if (persons.map((p) => p.name).includes(newPerson.name)) {
       alert(`${newPerson.name} is already added to phonebook`);
@@ -59,14 +66,29 @@ const App = () => {
     setPersons(persons.concat(newPerson));
   };
 
+  const [filter, setFilter] = useState('');
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const filteredPersons =
+    filter.length < 1
+      ? persons
+      : persons.filter((p) => p.name.toLowerCase().includes(filter.toLocaleLowerCase()));
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter:
+        <input type='text' value={filter} onChange={handleFilterChange} />
+      </div>
+      <h2>Add new</h2>
       <Form onAddNewPerson={handleNewPersonAdded} />
       <h2>Numbers</h2>
       <ul>
-        {persons.map((p) => (
-         <PersonItem key={p.name} person={p} />
+        {filteredPersons.map((p) => (
+          <PersonItem key={p.name} person={p} />
         ))}
       </ul>
     </div>
