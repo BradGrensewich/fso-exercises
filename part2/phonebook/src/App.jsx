37ed88f592b8f4953b3d71f121f11668714f6d34
BradGrensewich <bradgrensewich@gmail.com>
2025-10-15 15:52:30 +0700

    exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Form from './components/Form';
 import PersonList from './components/PersonList';
 import Filter from './components/Filter';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data)
+    })
+  }, [])
+
   const handleNewPersonAdded = (newPerson) => {
     if (persons.map((p) => p.name).includes(newPerson.name)) {
       alert(`${newPerson.name} is already added to phonebook`);
@@ -17,8 +21,7 @@ const App = () => {
     }
     setPersons(persons.concat(newPerson));
   };
-
-  const [filter, setFilter] = useState('');
+  
   const handleFilterChange = (event) => {
     setFilter(event.target.value);
   };
