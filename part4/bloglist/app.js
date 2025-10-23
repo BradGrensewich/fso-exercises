4405b52856bf29c26c141b6689ba2f6805d4ed37
BradGrensewich <bradgrensewich@gmail.com>
2025-10-23 10:56:08 +0700

    exercise 4.1

---
+++ part4/bloglist/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const Blog = require('./models/blog');
+
+const app = express();
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('Successfully connected server to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('Error connecting to MongoDB', error);
+  });
+
+app.use(express.json());
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+module.exports = app;
