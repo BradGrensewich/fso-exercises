fe4c1fc09011fb2c514774639fe5ec8ed73473c9
BradGrensewich <bradgrensewich@gmail.com>
2025-10-23 14:50:10 +0700

    exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+    const reducer = (sum, curr) => {
+        return sum + curr
+    }
+    return blogs.map(b => b.likes).reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy,
+  dummy, totalLikes
 };
