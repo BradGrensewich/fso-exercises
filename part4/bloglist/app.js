2880e64cbcb2b7f25bdb1dc420a726f12d02e4c5
BradGrensewich <bradgrensewich@gmail.com>
2025-10-28 14:44:44 +0700

    exercise 4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./utils/db');
 const middleware = require('./utils/middleware');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users')
 
 connectDB();
 const app = express();
@@ -10,6 +11,7 @@ app.use(middleware.requestLogger);
 
 //routes
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
