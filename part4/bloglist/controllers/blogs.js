412cbf43b71c058ec8ca6ffba2ade92849fa4b5c
BradGrensewich <bradgrensewich@gmail.com>
2025-10-29 13:10:06 +0700

    4.21

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,8 +1,16 @@
 const blogsRouter = require('express').Router();
 const User = require('../models/user');
 const Blog = require('../models/blog');
-const { request } = require('express');
 const jwt = require('jsonwebtoken');
+
+const getUserFromToken = async () => {
+  const decodedToken = jwt.decode(request.token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
+  return user;
+};
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
@@ -10,11 +18,7 @@ blogsRouter.get('/', async (request, response) => {
 });
 
 blogsRouter.post('/', async (request, response) => {
-  const decodedToken = jwt.decode(request.token, process.env.SECRET);
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' });
-  }
-  const user = await User.findById(decodedToken.id);
+  const user = await getUserFromToken();
   if (!user) {
     return response.status(400).json({ error: 'userId missing or not valid' });
   }
@@ -26,7 +30,14 @@ blogsRouter.post('/', async (request, response) => {
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id);
+  const user = await getUserFromToken();
+  const blog = await Blog.findById(request.params.id);
+  if (user.id.toString() !== blog.user.toString()) {
+    return response
+      .status(400)
+      .json({ error: 'user credentials not valid for delete of this blog' });
+  }
+  await blog.delete();
   return response.status(204).end();
 });
 
