b5d716c7c3d837b83511aaa27ed823157859ac0c
BradGrensewich <bradgrensewich@gmail.com>
2025-11-12 15:38:13 +0700

    exercise 5.18

--- part5/bloglist-tests/tests/bloglist_app.spec.js
+++ part5/bloglist-tests/tests/bloglist_app.spec.js
@@ -1,15 +1,35 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test');
+const helper = require('./helper');
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173');
+  beforeEach(async ({ page, request }) => {
+    page.goto('/');
+    await request.post('/api/testing/reset');
+    await request.post('/api/users', {
+      data: {
+        username: 'bradGrensewich',
+        password: 'secretword',
+        name: 'Brad',
+      },
+    });
   });
 
   test('Login form is shown', async ({ page }) => {
-    const titleEl = page.getByText('Log in');
-    await expect(titleEl).toBeVisible();
-    await expect(page.getByLabel('username')).toBeVisible()
-    await expect(page.getByLabel('password')).toBeVisible()
-    await expect(page.getByRole('button', {name: 'login'})).toBeVisible()
+    await expect(page.getByText('Log in')).toBeVisible();
+    await expect(page.getByLabel('username')).toBeVisible();
+    await expect(page.getByLabel('password')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'login' })).toBeVisible();
+  });
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await helper.loginWith(page, 'bradGrensewich', 'secretword');
+      await expect(page.getByText('bradGrensewich is logged in')).toBeVisible();
+    });
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await helper.loginWith(page, 'bradGrensewich', 'wrongpassword');
+      await expect(page.getByText('bradGrensewich is logged in')).not.toBeVisible()
+    });
   });
 });
