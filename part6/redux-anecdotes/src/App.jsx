fd9549d21eb91e1c8981231f1350cb970fca5abf
BradGrensewich <bradgrensewich@gmail.com>
2025-11-19 16:16:02 +0700

    exercise 6.4

---
+++ part6/redux-anecdotes/src/App.jsx
@@ -0,0 +1,28 @@
+import { useSelector } from 'react-redux';
+import AnecdoteForm from './components/AnecdoteForm';
+
+const App = () => {
+  const anecdotes = useSelector((state) => state);
+
+  const vote = (id) => {
+    console.log('vote', id);
+  };
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+      <AnecdoteForm />
+    </div>
+  );
+};
+
+export default App;
