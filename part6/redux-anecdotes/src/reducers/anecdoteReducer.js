394fc82ac11f8807733a3e34e9d51c34b6f2a28a
BradGrensewich <bradgrensewich@gmail.com>
2025-11-22 15:32:14 +0700

    exercise 6.15

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,14 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const getId = () => (100000 * Math.random()).toFixed(0);
 
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0,
-  };
-};
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
@@ -21,7 +13,7 @@ const anecdoteSlice = createSlice({
       return state.map((a) => (a.id === id ? changed : a));
     },
     createAnecdote(state, action) {
-      state.push(asObject(action.payload));
+      state.push(action.payload);
     },
     setAnecdotes(state, action) {
       return action.payload
