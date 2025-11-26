9a80d2e55e3b0ff129ae417f4a22265a1a405d49
BradGrensewich <bradgrensewich@gmail.com>
2025-11-26 16:26:57 +0700

    exercise 7.6

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -1,9 +1,11 @@
 import { useField } from '../hooks';
 
 const CreateNew = ({ addNew }) => {
-  const content = useField('text');
-  const author = useField('text');
-  const info = useField('text');
+  const {reset: contentReset , ...content} = useField('text');
+  const {reset: authorReset , ...author} = useField('text');
+  const {reset: infoReset , ...info} = useField('text');
+
+ 
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,9 +19,9 @@ const CreateNew = ({ addNew }) => {
 
   const handleReset = (e) => {
     e.preventDefault();
-    content.reset();
-    author.reset();
-    info.reset();
+    contentReset()
+    authorReset();
+    infoReset();
   };
 
   return (
